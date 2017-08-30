d883a1c878037458f5d77e30084208ec32449d90
remijouannet <remijouannet@gmail.com>
2017-08-31 01:48:32 +0200

    new release script

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ Download the binary and put it in the same folder than terraform binary
 ```
 $ wget https://github.com/remijouannet/terraform-provider-osc/releases/download/v0.1/terraform-provider-osc_darwin_amd64_v0.1.zip
 $ unzip terraform-provider-osc_darwin_amd64_v0.1.zip
-$ cp terraform-provider-osc_darwin_amd64_v0.1 $(dirname $(which terraform))
+$ cp terraform-provider-osc_darwin_amd64_v0.1/terraform-provider-osc_v0.1.zip $(dirname $(which terraform))
 $ chmod +x $(dirname $(which terraform))/terraform-provider-osc_darwin_amd64_v0.1
 ```
 
--- scripts/github-releases.sh
+++ scripts/github-releases.sh
@@ -6,7 +6,23 @@ version=$(git describe --abbrev=0 --tags)
 
 which github-release || echo 'please install the tool github-releases'
 
-github-release info -u remijouannet -r terraform-provider-osc -t $version || echo "the release doesn't exist"
+github-release info \
+    --user remijouannet \
+    --repo terraform-provider-osc \
+    --tag $version || echo "the release doesn't exist"
+
+if [ $? != 0 ]
+then
+    github-release release \
+        --user remijouannet \
+        --repo terraform-provider-osc \
+        --tag $version \
+        --draft \
+        --pre-release \
+        --name "$version-hyper-alpha-yolo-experimental" \
+        --description "risks of explosion" \
+        --target $version || echo "failed to create release for $version"
+if
 
 cd pkg/
 
@@ -18,10 +34,10 @@ do
     zip $binary.zip $binary
     echo "upload $binary"
     github-release upload \
-        -u remijouannet \
+        --user remijouannet \
         --name "$binary.zip" \
-        -r terraform-provider-osc \
-        -f "$binary.zip" \
+        --repo terraform-provider-osc \
+        --file "$binary.zip" \
         --replace \
-        -t $version || echo "failed to upload $binary"
+        --tag $version || echo "failed to upload $binary"
 done
