59de7be61027cff95fc1a14e3df3294f9d20b26f
Rémi Jouannet <remi.jouannet@outscale.com>
2017-12-15 10:35:43 +0100

    add depends_on for natgw creation

--- examples/elb-vpc/natgw.tf
+++ examples/elb-vpc/natgw.tf
@@ -1,4 +1,5 @@
 resource "osc_nat_gateway" "euw2-core-1-natgw-1" {
   allocation_id = "${osc_eip.euw2-core-1-eip-1.id}"
   subnet_id     = "${osc_subnet.euw2-core-1-subnet-1.id}"
+  depends_on    = ["osc_route_table_association.euw2-core-1-route-1"]
 }
--- examples/vpc-peering/natgw.tf
+++ examples/vpc-peering/natgw.tf
@@ -1,9 +1,11 @@
 resource "osc_nat_gateway" "euw2-core-1-natgw-1" {
   allocation_id = "${osc_eip.euw2-core-1-eip-1.id}"
   subnet_id     = "${osc_subnet.euw2-core-1-subnet-1.id}"
+  depends_on    = ["osc_route_table_association.euw2-core-1-route-1"]
 }
 
 resource "osc_nat_gateway" "euw2-core-2-natgw-1" {
   allocation_id = "${osc_eip.euw2-core-2-eip-1.id}"
   subnet_id     = "${osc_subnet.euw2-core-2-subnet-1.id}"
+  depends_on    = ["osc_route_table_association.euw2-core-2-route-1"]
 }
